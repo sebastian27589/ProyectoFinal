fb82d8fb4fde6818350d335f977e3931cbe7b034
sebastian27589 <smrm0002@ce.pucmm.edu.do>
2023-11-23 15:20:30 -0400

    Cambio de la clase Medico.

--- ProyectoFinalClinica/src/logico/Medico.java
+++ ProyectoFinalClinica/src/logico/Medico.java
@@ -1,19 +1,15 @@
 package logico;
 
-import java.util.ArrayList;
-
 public class Medico extends Persona {
 
 	private String codeMedico;
 	private String especialidad;
-	private ArrayList<Cita> citasPendientes;
 	
 	public Medico(String cedula, String nombre, char sexo, String telefono, String direccion, String codeMedico,
 			String especialidad) {
 		super(cedula, nombre, sexo, telefono, direccion);
 		this.codeMedico = codeMedico;
 		this.especialidad = especialidad;
-		this.citasPendientes = new ArrayList<Cita>();
 	}
 
 	public String getCodeMedico() {
@@ -30,14 +26,6 @@ public class Medico extends Persona {
 
 	public void setEspecialidad(String especialidad) {
 		this.especialidad = especialidad;
-	}
-
-	public ArrayList<Cita> getCitasPendientes() {
-		return citasPendientes;
-	}
-
-	public void setCitasPendientes(ArrayList<Cita> citasPendientes) {
-		this.citasPendientes = citasPendientes;
 	}
 	
 }
